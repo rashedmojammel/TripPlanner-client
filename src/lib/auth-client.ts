f73c2c71a7db4f8cd3e54520e3eb18a8f6396ed8
rashedmojammel <rashedmojamme56@gmail.com>
2026-07-19 12:51:39 +0600

    fixed depoloy issue

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:5000/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL!;
 
 export const authClient = createAuthClient({
   baseURL: API_URL.replace(/\/api$/, "") + "/api/auth",
